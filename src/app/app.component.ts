97e661c593cdef18ce9f32c63ce9b06624fa3173
Joel Messerli <hi.github@peg.nu>
2020-06-03 11:02:10 +0200

    Show image expiration in ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
 import {Image} from './image.model';
 import {KeycloakService} from 'keycloak-angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -19,12 +20,12 @@ export class AppComponent {
   constructor(private http: HttpClient,
               private keycloak: KeycloakService) {
     this.backgroundImage$ = http.get<any>('https://peg.nu/api/unsplash/image').pipe(
-      map(img => new Image(img.image_url, img.photographer_name, img.photographer_username)),
-      shareReplay(1)
+      map(img => new Image(img.image_url, img.photographer_name, img.photographer_username, img.updated_at, img.expiration_duration)),
+      shareReplay(1),
     );
 
     this.backgroundUrlCss$ = this.backgroundImage$.pipe(
-      map((img: Image) => `url(${img.imageUrl})`)
+      map((img: Image) => `url(${img.imageUrl})`),
     );
 
     this.isImageClearingAllowed = keycloak.getUserRoles().filter(role => role === 'PegNu-Short.CLEAR-BACKGROUND').length > 0;
@@ -34,7 +35,7 @@ export class AppComponent {
     this.imageReloading = true;
 
     this.http.get<any>('https://peg.nu/api/unsplash/clear').pipe(
-      map(res => res.status === 'ok')
+      map(res => res.status === 'ok'),
     ).subscribe(success => {
       if (!success) {
         this.imageReloading = false;
@@ -43,5 +44,9 @@ export class AppComponent {
 
       window.location.reload();
     });
+  }
+
+  getRelativeExpirationTime(updateTime: string, expirationDuration: number): string {
+    return moment(updateTime).add(expirationDuration / 1000000, 'ms').fromNow();
   }
 }
--- src/app/image.model.ts
+++ src/app/image.model.ts
@@ -1,6 +1,8 @@
 export class Image {
   constructor(public imageUrl: string,
               public photographerName: string,
-              public photographerUsername: string) {
+              public photographerUsername: string,
+              public updatedAt: string,
+              public expireDuration: number) {
   }
 }
