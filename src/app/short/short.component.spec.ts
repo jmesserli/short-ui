02e69972254905e50373aa7f7f1a4b5ce1e94821
Joel Messerli <hi.github@peg.nu>
2020-12-17 21:57:13 +0100

    Update angular

--- src/app/short/short.component.spec.ts
+++ src/app/short/short.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ShortComponent } from './short.component';
 
@@ -6,7 +6,7 @@ describe('ShortComponent', () => {
   let component: ShortComponent;
   let fixture: ComponentFixture<ShortComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ShortComponent ]
     })
