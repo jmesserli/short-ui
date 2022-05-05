13b2fc90e181db1c703a54ed598dc87263035123
Joel Messerli <git@pegnu.dev>
2022-05-05 17:30:54 +0200

    config(prd): use old idp for now

--- src/app/config/prd-config.ts
+++ src/app/config/prd-config.ts
@@ -2,9 +2,9 @@ import { Config } from '../services/config.service';
 
 export const config: Config = {
   keycloakConfig: {
-    clientId: 'pegnu-short',
-    url: 'https://auth.pegnu.dev',
-    realm: 'pegnu',
+    clientId: 'short',
+    url: 'https://id.peg.nu',
+    realm: 'PegNu',
   },
   analyticsConfig: {
     plausible: {
