45a958d22097105f398ab03c279903ce0ebf8a8d
Joel Messerli <git@pegnu.dev>
2023-02-22 22:23:03 +0100

    chore: update angular+material to v15

--- src/app/pages/user-links/user-links.component.ts
+++ src/app/pages/user-links/user-links.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ShortService } from '../../services/short.service';
 import { Link } from '../../model/link.model';
 import { filter, map, switchMap } from 'rxjs/operators';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
+import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatDialog } from '@angular/material/dialog';
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 import { ConfirmDeleteLinkComponent } from '../../components/confirm-delete-link-dialog/confirm-delete-link.component';
 
 @Component({
